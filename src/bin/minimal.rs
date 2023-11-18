4dc2f4653cdb143f91ff7d36d2350bea16e077f8
90degs2infty <90degs2infty@posteo.org>
2023-11-18 17:41:28 +0100

    chore: set up high level timer to drive display

--- src/bin/minimal.rs
+++ src/bin/minimal.rs
@@ -11,35 +11,48 @@ use microtile_app as _; // global logger + panicking-behavior + memory layout
     dispatchers = [SWI0_EGU0]
 )]
 mod app {
+    use microbit::{
+        hal::{
+            prelude::_embedded_hal_timer_CountDown,
+            timer::{Instance, Periodic, Timer},
+        },
+        pac::{NVIC, TIMER1 as HighLevelDisplayDriver},
+        Board,
+    };
+
+    const HIGH_LEVEL_DISPLAY_FREQ: u32 = 10;
+    const HIGH_LEVEL_DISPLAY_CYCLES: u32 =
+        Timer::<HighLevelDisplayDriver, Periodic>::TICKS_PER_SECOND / HIGH_LEVEL_DISPLAY_FREQ;
+
     // Shared resources go here
     #[shared]
-    struct Shared {
-        // TODO: Add resources
-    }
+    struct Shared {}
 
     // Local resources go here
     #[local]
     struct Local {
-        // TODO: Add resources
+        highlevel_display_driver: Timer<HighLevelDisplayDriver, Periodic>,
     }
 
     #[init]
     fn init(cx: init::Context) -> (Shared, Local) {
         defmt::info!("init");
 
-        // TODO setup monotonic if used
-        // let sysclk = { /* clock setup + returning sysclk as an u32 */ };
-        // let token = rtic_monotonics::create_systick_token!();
-        // rtic_monotonics::systick::Systick::new(cx.core.SYST, sysclk, token);
+        let board = Board::new(cx.device, cx.core);
 
-        task1::spawn().ok();
+        // Configure timer to generate an IRQ at frequency HIGH_LEVEL_DISPLAY_FREQ
+        let mut highlevel_display = Timer::periodic(board.TIMER1);
+        highlevel_display.enable_interrupt();
+        highlevel_display.start(HIGH_LEVEL_DISPLAY_CYCLES);
+
+        unsafe {
+            NVIC::unmask(HighLevelDisplayDriver::INTERRUPT);
+        }
 
         (
-            Shared {
-                // Initialization of shared resources go here
-            },
+            Shared {},
             Local {
-                // Initialization of local resources go here
+                highlevel_display_driver: highlevel_display,
             },
         )
     }
@@ -49,14 +62,13 @@ mod app {
     fn idle(_: idle::Context) -> ! {
         defmt::info!("idle");
 
-        loop {
-            continue;
-        }
+        #[allow(clippy::empty_loop)]
+        loop {}
     }
 
-    // TODO: Add tasks
-    #[task(priority = 1)]
-    async fn task1(_cx: task1::Context) {
-        defmt::info!("Hello from task1!");
+    #[task(binds = TIMER1, priority = 1, local = [ highlevel_display_driver ])]
+    fn high_level_tick_display(cx: high_level_tick_display::Context) {
+        defmt::info!("tick");
+        cx.local.highlevel_display_driver.reset_event();
     }
 }
