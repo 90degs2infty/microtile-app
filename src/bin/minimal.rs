2639710bc56ce756b321242815cb77b006811bb3
90degs2infty <90degs2infty@posteo.org>
2023-11-27 20:23:11 +0100

    Update frames on game change

--- src/bin/minimal.rs
+++ src/bin/minimal.rs
@@ -32,39 +32,57 @@ mod app {
         game::{Game, TileFloating},
         raster::{Active, Passive, RasterizationExt},
     };
+    use microtile_engine::{gameplay::game::Observer, geometry::grid::Grid};
     use rtic_sync::channel::{Channel, TrySendError};
 
     const HIGH_LEVEL_DISPLAY_FREQ: u32 = 5;
     const HIGH_LEVEL_DISPLAY_CYCLES: u32 =
         Timer::<HighLevelDisplayDriver, Periodic>::TICKS_PER_SECOND / HIGH_LEVEL_DISPLAY_FREQ;
 
+    #[derive(Debug)]
+    struct GameObserver;
+
+    impl Observer for GameObserver {
+        fn signal_board_changed(&self, active: Grid, passive: Grid) {
+            // When processing the topmost row, there are two signals comming in at short distance,
+            // because processing the last row triggers a signal and placing a new tile triggers a signal too
+            match update_frames::spawn(active, passive) {
+                Ok(()) => {}
+                Err(_) => defmt::debug!("dropping board update to allow for hardware to catch up"),
+            }
+        }
+    }
+
     // Shared resources go here
     #[shared]
     struct Shared {
         display: Display<LowLevelDisplayDriver>,
+        merged_frame: MicrobitFrame,
+        passive_frame: MicrobitFrame,
     }
 
     // Local resources go here
     #[local]
     struct Local {
         highlevel_display_driver: Timer<HighLevelDisplayDriver, Periodic>,
-        passive_frame: MicrobitFrame,
-        merged_frame: MicrobitFrame,
         game_driver: &'static mut GameDriver<'static, TimerGameDriver>,
         game_driver_timer_handler: &'static mut TimerHandler<'static, TimerGameDriver, Periodic>,
     }
 
-    #[init(local = [ timer_channel: Channel<Message, MAILBOX_CAPACITY> = Channel::new(), game_driver_mem: MaybeUninit<GameDriver<'static, TimerGameDriver>> = MaybeUninit::uninit(), game_driver_timer_handler_mem: MaybeUninit<TimerHandler<'static, TimerGameDriver, Periodic>> = MaybeUninit::uninit() ])]
+    #[init(local = [ timer_channel: Channel<Message, MAILBOX_CAPACITY> = Channel::new(), game_driver_mem: MaybeUninit<GameDriver<'static, TimerGameDriver, GameObserver>> = MaybeUninit::uninit(), game_driver_timer_handler_mem: MaybeUninit<TimerHandler<'static, TimerGameDriver, Periodic>> = MaybeUninit::uninit() ])]
     fn init(cx: init::Context) -> (Shared, Local) {
         defmt::info!("init");
 
         let board = Board::new(cx.device, cx.core);
+
+        let observer = GameObserver {};
 
         cx.local.game_driver_mem.write(GameDriver::new(
             board.buttons.button_a,
             board.buttons.button_b,
             board.TIMER2,
             cx.local.timer_channel,
+            observer,
         ));
         let game_driver = unsafe { cx.local.game_driver_mem.assume_init_mut() };
         let game_driver_timer_handler = game_driver
@@ -77,15 +95,9 @@ mod app {
             unsafe { cx.local.game_driver_timer_handler_mem.assume_init_mut() };
 
         let game = game::initialize_dummy();
-        let passive_grid = <Game<TileFloating> as RasterizationExt<Passive>>::rasterize(&game);
 
-        let mut passive_frame = MicrobitFrame::default();
-        passive_frame.set(&GridRenderer::new(&passive_grid));
-
-        let mut merged_frame = MicrobitFrame::default();
-        merged_frame.set(&GridRenderer::new(&passive_grid.union(
-            <Game<TileFloating> as RasterizationExt<Active>>::rasterize(&game),
-        )));
+        let passive_frame = MicrobitFrame::default();
+        let merged_frame = MicrobitFrame::default();
 
         // Configure timer to generate an IRQ at frequency HIGH_LEVEL_DISPLAY_FREQ
         let mut highlevel_display = Timer::periodic(board.TIMER1);
@@ -99,11 +111,11 @@ mod app {
         (
             Shared {
                 display: Display::new(board.TIMER0, board.display_pins),
+                merged_frame,
+                passive_frame,
             },
             Local {
                 highlevel_display_driver: highlevel_display,
-                passive_frame,
-                merged_frame,
                 game_driver,
                 game_driver_timer_handler,
             },
@@ -133,18 +145,16 @@ mod app {
         };
     }
 
-    #[task(priority = 1, local = [ next_frame_passive: bool = false, passive_frame, merged_frame ], shared = [ display ])]
-    async fn display_toggle_frame(mut cx: display_toggle_frame::Context) {
-        let next_frame = if *cx.local.next_frame_passive {
-            cx.local.passive_frame
+    #[task(priority = 1, local = [ next_frame_passive: bool = false ], shared = [ display, passive_frame, merged_frame ])]
+    async fn display_toggle_frame(cx: display_toggle_frame::Context) {
+        if *cx.local.next_frame_passive {
+            (cx.shared.display, cx.shared.passive_frame)
+                .lock(|display, frame| display.show_frame(frame))
         } else {
-            cx.local.merged_frame
-        };
+            (cx.shared.display, cx.shared.merged_frame)
+                .lock(|display, frame| display.show_frame(frame))
+        }
         *cx.local.next_frame_passive = !*cx.local.next_frame_passive;
-
-        cx.shared.display.lock(|display| {
-            display.show_frame(next_frame);
-        })
     }
 
     #[task(binds = TIMER0, priority = 4, shared = [ display ])]
@@ -168,5 +178,15 @@ mod app {
             }
             Err(TrySendError::NoReceiver(_)) => unreachable!(),
         };
+    }
+
+    #[task(priority = 1, shared = [ merged_frame, passive_frame ])]
+    async fn update_frames(cx: update_frames::Context, active: Grid, passive: Grid) {
+        defmt::trace!("entering frame update");
+        (cx.shared.merged_frame, cx.shared.passive_frame).lock(|merged_frame, passive_frame| {
+            passive_frame.set(&GridRenderer::new(&passive));
+            merged_frame.set(&GridRenderer::new(&active.union(passive)));
+        });
+        defmt::trace!("leaving frame update");
     }
 }
