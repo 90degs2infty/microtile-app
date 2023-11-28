e0caefe9d7640d761a0ae6b27015935f1e6a1320
90degs2infty <121056900+90degs2infty@users.noreply.github.com>
2023-11-28 18:03:01 +0100

    feat: setup basic game driver (#8)
    The merged code requires some refactoring after implementing the game
    observer from #7 .

--- src/bin/minimal.rs
+++ src/bin/minimal.rs
@@ -11,24 +11,33 @@ use microtile_app as _; // global logger + panicking-behavior + memory layout
     dispatchers = [SWI0_EGU0]
 )]
 mod app {
+    use core::mem::MaybeUninit;
     use microbit::{
-        display::nonblocking::{Display, Frame, MicrobitFrame},
+        display::nonblocking::{Display, MicrobitFrame},
         hal::{
             prelude::_embedded_hal_timer_CountDown,
             timer::{Instance, Periodic, Timer},
         },
-        pac::{NVIC, TIMER0 as LowLevelDisplayDriver, TIMER1 as HighLevelDisplayDriver},
+        pac::{
+            NVIC, TIMER0 as LowLevelDisplayDriver, TIMER1 as HighLevelDisplayDriver,
+            TIMER2 as TimerGameDriver,
+        },
         Board,
     };
-    use microtile_app::{device::display::GridRenderer, game};
-    use microtile_engine::gameplay::{
-        game::{Game, TileFloating},
-        raster::{Active, Passive, RasterizationExt},
-    };
+    use microtile_app::game::{GameDriver, Message, TimerHandler, MAILBOX_CAPACITY};
+    use microtile_engine::{gameplay::game::Observer, geometry::grid::Grid};
+    use rtic_sync::channel::{Channel, TrySendError};
 
     const HIGH_LEVEL_DISPLAY_FREQ: u32 = 5;
     const HIGH_LEVEL_DISPLAY_CYCLES: u32 =
         Timer::<HighLevelDisplayDriver, Periodic>::TICKS_PER_SECOND / HIGH_LEVEL_DISPLAY_FREQ;
+
+    #[derive(Debug)]
+    struct NoopObserver;
+
+    impl Observer for NoopObserver {
+        fn signal_board_changed(&self, _: Grid, _: Grid) {}
+    }
 
     // Shared resources go here
     #[shared]
@@ -40,26 +49,41 @@ mod app {
     #[local]
     struct Local {
         highlevel_display_driver: Timer<HighLevelDisplayDriver, Periodic>,
-        passive_frame: MicrobitFrame,
+        game_driver: &'static mut GameDriver<'static, TimerGameDriver, NoopObserver>,
+        game_driver_timer_handler: &'static mut TimerHandler<'static, TimerGameDriver, Periodic>,
         merged_frame: MicrobitFrame,
+        passive_frame: MicrobitFrame,
     }
 
-    #[init]
+    #[init(local = [ timer_channel: Channel<Message, MAILBOX_CAPACITY> = Channel::new(), game_driver_mem: MaybeUninit<GameDriver<'static, TimerGameDriver, NoopObserver>> = MaybeUninit::uninit(), game_driver_timer_handler_mem: MaybeUninit<TimerHandler<'static, TimerGameDriver, Periodic>> = MaybeUninit::uninit() ])]
     fn init(cx: init::Context) -> (Shared, Local) {
         defmt::info!("init");
 
         let board = Board::new(cx.device, cx.core);
 
-        let game = game::initialize_dummy();
-        let passive_grid = <Game<TileFloating> as RasterizationExt<Passive>>::rasterize(&game);
+        let observer = NoopObserver {};
 
-        let mut passive_frame = MicrobitFrame::default();
-        passive_frame.set(&GridRenderer::new(&passive_grid));
+        cx.local.game_driver_mem.write(GameDriver::new(
+            board.buttons.button_a,
+            board.buttons.button_b,
+            board.TIMER2,
+            cx.local.timer_channel,
+            observer,
+        ));
+        let game_driver = unsafe { cx.local.game_driver_mem.assume_init_mut() };
+        let game_driver_timer_handler = game_driver
+            .get_timer_handler()
+            .expect("freshly initialized driver should still have handler available");
+        cx.local
+            .game_driver_timer_handler_mem
+            .write(game_driver_timer_handler);
+        let game_driver_timer_handler =
+            unsafe { cx.local.game_driver_timer_handler_mem.assume_init_mut() };
 
-        let mut merged_frame = MicrobitFrame::default();
-        merged_frame.set(&GridRenderer::new(&passive_grid.union(
-            <Game<TileFloating> as RasterizationExt<Active>>::rasterize(&game),
-        )));
+        drive_game::spawn().ok();
+
+        let passive_frame = MicrobitFrame::default();
+        let merged_frame = MicrobitFrame::default();
 
         // Configure timer to generate an IRQ at frequency HIGH_LEVEL_DISPLAY_FREQ
         let mut highlevel_display = Timer::periodic(board.TIMER1);
@@ -76,8 +100,10 @@ mod app {
             },
             Local {
                 highlevel_display_driver: highlevel_display,
-                passive_frame,
+                game_driver,
+                game_driver_timer_handler,
                 merged_frame,
+                passive_frame,
             },
         )
     }
@@ -105,18 +131,14 @@ mod app {
         };
     }
 
-    #[task(priority = 1, local = [ next_frame_passive: bool = false, passive_frame, merged_frame ], shared = [ display ])]
+    #[task(priority = 1, local = [ next_frame_passive: bool = false , passive_frame, merged_frame ], shared = [ display ])]
     async fn display_toggle_frame(mut cx: display_toggle_frame::Context) {
-        let next_frame = if *cx.local.next_frame_passive {
-            cx.local.passive_frame
+        if *cx.local.next_frame_passive {
+            (cx.shared.display).lock(|display| display.show_frame(&cx.local.passive_frame))
         } else {
-            cx.local.merged_frame
-        };
+            (cx.shared.display).lock(|display| display.show_frame(&cx.local.merged_frame))
+        }
         *cx.local.next_frame_passive = !*cx.local.next_frame_passive;
-
-        cx.shared.display.lock(|display| {
-            display.show_frame(next_frame);
-        })
     }
 
     #[task(binds = TIMER0, priority = 4, shared = [ display ])]
@@ -124,5 +146,22 @@ mod app {
         cx.shared.display.lock(|display| {
             display.handle_display_event();
         });
+    }
+
+    #[task(priority = 1, local = [ game_driver ])]
+    async fn drive_game(cx: drive_game::Context) {
+        defmt::trace!("driving the game now");
+        let _ = cx.local.game_driver.run().await;
+    }
+
+    #[task(binds = TIMER2, priority = 4, local = [ game_driver_timer_handler ])]
+    fn tick_game(cx: tick_game::Context) {
+        match cx.local.game_driver_timer_handler.handle_timer_event() {
+            Ok(()) => {}
+            Err(TrySendError::Full(_)) => {
+                defmt::debug!("dropping game tick to allow the engine to catch up")
+            }
+            Err(TrySendError::NoReceiver(_)) => unreachable!(),
+        };
     }
 }
--- src/game.rs
+++ src/game.rs
@@ -1,14 +1,182 @@
+use core::fmt::Debug;
+use either::Either;
+use microbit::{
+    gpio::{BTN_A, BTN_B},
+    hal::{
+        prelude::_embedded_hal_timer_CountDown,
+        timer::{Instance, Periodic, Timer},
+    },
+};
 use microtile_engine::{
-    gameplay::game::{Game, TileFloating},
+    gameplay::game::{Game, Observer, ProcessRows, TileFloating},
     geometry::tile::BasicTile,
 };
+use rtic_sync::channel::{Channel, ReceiveError, Receiver, Sender, TrySendError};
 
-pub fn initialize_dummy() -> Game<TileFloating> {
-    Game::default()
-        .place_tile(BasicTile::Diagonal)
-        .expect_left("Game should not have ended by this first tile")
-        .descend_tile()
-        .expect_left("Tile should still be floating")
-        .descend_tile()
-        .expect_left("Tile should still be floating")
+pub enum Message {
+    TimerTick,
+    BtnAPress,
+    BtnBPress,
 }
+
+pub enum DriverError {
+    SenderDropped,
+}
+
+// TODO: I don't like the fact that I'm handing out the timer at this point,
+// this makes shutting down the timer kind of hard - also starting the timer in
+// the run function is not possible
+pub struct TimerHandler<'a, T, U> {
+    mailbox: Sender<'a, Message, MAILBOX_CAPACITY>,
+    timer: Timer<T, U>,
+}
+
+impl<'a, T, U> TimerHandler<'a, T, U>
+where
+    T: Instance,
+{
+    fn new(mailbox: Sender<'a, Message, MAILBOX_CAPACITY>, timer: Timer<T, U>) -> Self {
+        Self { mailbox, timer }
+    }
+
+    pub fn handle_timer_event(&mut self) -> Result<(), TrySendError<Message>> {
+        self.timer.reset_event();
+        self.mailbox.try_send(Message::TimerTick)
+    }
+}
+
+enum State<O> {
+    _ProcessRows(Game<ProcessRows, O>),
+    _TileFloating(Game<TileFloating, O>),
+    /// Dummy value which is just there to apply [Jone's trick](https://matklad.github.io/2019/07/25/unsafe-as-a-type-system.html)
+    _Processing,
+}
+
+impl<O> State<O> {
+    fn apply<F>(&mut self, f: F)
+    where
+        F: FnOnce(Self) -> Self,
+    {
+        let stolen = core::mem::replace(self, Self::_Processing);
+        *self = f(stolen);
+    }
+}
+
+pub struct GameDriver<'a, T, O> {
+    _s: State<O>,
+    _button_a: BTN_A,
+    _button_b: BTN_B,
+    _timer_sender: Sender<'a, Message, MAILBOX_CAPACITY>,
+    _timer_receiver: Receiver<'a, Message, MAILBOX_CAPACITY>,
+    _timer_handler: Option<TimerHandler<'a, T, Periodic>>,
+}
+
+/// This mailbox capacity belongs to [`GameDriver`], but since [`GameDriver`] is
+/// generic and
+/// `generic 'Self' types are currently not permitted in anonymous constants` (`rustc` error message)
+/// the capacity is defined as free constant.
+pub const MAILBOX_CAPACITY: usize = 4;
+
+impl<'a, T, O> GameDriver<'a, T, O>
+where
+    T: Instance,
+    O: Observer + Debug,
+{
+    const GAME_TICK_FREQ: u32 = 1; // TODO: make this 2 as soon as you implement softdrop
+    const GAME_TICK_CYCLES: u32 = Timer::<T, Periodic>::TICKS_PER_SECOND / Self::GAME_TICK_FREQ;
+
+    /// Note: the contained peripherals start generating events right away, so be sure to
+    /// set up the event handling as fast as possible
+    pub fn new(
+        _button_a: BTN_A,
+        _button_b: BTN_B,
+        _timer: T,
+        _timer_channel: &'a mut Channel<Message, MAILBOX_CAPACITY>,
+        _o: O,
+    ) -> Self {
+        // initialize the game
+        let mut game = Game::default()
+            .place_tile(BasicTile::Diagonal)
+            .expect_left("the first tile should not end the game");
+        game.set_observer(_o)
+            .expect("newly initialized game should not have observer set");
+
+        // initialize the timer
+        let mut game_tick = Timer::periodic(_timer);
+        game_tick.reset_event(); // out of caution
+        game_tick.enable_interrupt();
+        game_tick.start(Self::GAME_TICK_CYCLES);
+
+        let (sender, receiver) = _timer_channel.split();
+
+        Self {
+            _s: State::_TileFloating(game),
+            _button_a,
+            _button_b,
+            _timer_sender: sender.clone(),
+            _timer_receiver: receiver,
+            _timer_handler: Some(TimerHandler::new(sender.clone(), game_tick)),
+        }
+    }
+
+    pub async fn run(&mut self) -> Result<(), DriverError> {
+        loop {
+            let msg = self._timer_receiver.recv().await.map_err(|e| match e {
+                ReceiveError::Empty => unreachable!(""),
+                ReceiveError::NoSender => DriverError::SenderDropped,
+            })?;
+            defmt::debug!(
+                "consuming message, more messages pending: {}",
+                !self._timer_receiver.is_empty()
+            );
+
+            match msg {
+                Message::TimerTick => self._s.apply(|state| match state {
+                    State::_TileFloating(game) => match game.descend_tile() {
+                        Either::Left(game) => State::_TileFloating(game),
+                        Either::Right(game) => State::_ProcessRows(game),
+                    },
+                    State::_ProcessRows(game) => match game.process_row() {
+                        Either::Left(game) => State::_ProcessRows(game),
+                        Either::Right(game) => match game.place_tile(BasicTile::Diagonal) {
+                            Either::Left(game) => State::_TileFloating(game),
+                            Either::Right(mut game) => {
+                                defmt::info!("restarting game");
+                                let o = game
+                                    .clear_observer()
+                                    .expect("game should have an observer set");
+                                let mut game = Game::default();
+                                game.set_observer(o)
+                                    .expect("newly initialized game should not have observer set");
+                                let game = game
+                                    .place_tile(BasicTile::Diagonal)
+                                    .expect_left("first tile should not end game");
+                                State::_TileFloating(game)
+                            }
+                        },
+                    },
+                    State::_Processing => {
+                        unreachable!("_Processing should be an intermediate value")
+                    }
+                }),
+                Message::BtnAPress => todo!(),
+                Message::BtnBPress => todo!(),
+            }
+        }
+    }
+
+    pub fn get_timer_handler(&mut self) -> Option<TimerHandler<'a, T, Periodic>> {
+        self._timer_handler.take()
+    }
+
+    pub fn return_timer_handler(&mut self, handler: TimerHandler<'a, T, Periodic>) {
+        if self._timer_handler.is_some() {
+            unreachable!(
+                "There can be at most one object of type TimerHandler over the programs entire lifetime"
+            )
+        }
+        self._timer_handler = Some(handler);
+    }
+}
+
+// https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=8ca99994a288cf4728778f0fa70f8d16
