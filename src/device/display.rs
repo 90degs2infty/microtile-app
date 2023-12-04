3dcf82b02832dec330a174e5959bf5f3c283132f
90degs2infty <90degs2infty@posteo.org>
2023-12-04 17:13:18 +0100

    Document changes

--- src/device/display.rs
+++ src/device/display.rs
@@ -15,6 +15,9 @@ impl<'a> GridRenderer<'a> {
 
 impl<'a> Render for GridRenderer<'a> {
     fn brightness_at(&self, x: usize, y: usize) -> u8 {
+        // For the `brightness_at` function, (0, 0) is in the top-left corner whereas for the game
+        // engine, (0, 0) is in the lower-left corner. Consequently, we have to invert the direction
+        // of the rows' index and offset the index by its maximum value.
         if let Ok(set) = self.0.is_element_set(Self::ROW_TRANSLATION_OFFSET - y, x) {
             if set {
                 return MAX_BRIGHTNESS;
