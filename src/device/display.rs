e2d084ea0c5e6ef3d5eeb97671ca340ab78179e7
90degs2infty <90degs2infty@posteo.org>
2023-12-04 17:11:06 +0100

    Offset and invert row indexing

--- src/device/display.rs
+++ src/device/display.rs
@@ -1,9 +1,12 @@
+use microbit::display::nonblocking::MicrobitFrame;
 use microtile_engine::geometry::grid::Grid;
-use tiny_led_matrix::{Render, MAX_BRIGHTNESS};
+use tiny_led_matrix::{Frame, Matrix, Render, MAX_BRIGHTNESS};
 
 pub struct GridRenderer<'a>(&'a Grid);
 
 impl<'a> GridRenderer<'a> {
+    const ROW_TRANSLATION_OFFSET: usize = <<MicrobitFrame as Frame>::Mtx as Matrix>::IMAGE_ROWS - 1;
+
     #[must_use]
     pub fn new(grid: &'a Grid) -> Self {
         Self(grid)
@@ -12,7 +15,7 @@ impl<'a> GridRenderer<'a> {
 
 impl<'a> Render for GridRenderer<'a> {
     fn brightness_at(&self, x: usize, y: usize) -> u8 {
-        if let Ok(set) = self.0.is_element_set(y, x) {
+        if let Ok(set) = self.0.is_element_set(Self::ROW_TRANSLATION_OFFSET - y, x) {
             if set {
                 return MAX_BRIGHTNESS;
             }
