1fbd15a509b6db5e93e278e3e15c3cf1b2a61903
90degs2infty <90degs2infty@posteo.org>
2023-12-04 16:45:18 +0100

    Collect all gpio-related resources in single struct

--- src/device/button.rs
+++ src/device/button.rs
@@ -1,8 +1,11 @@
 use crate::game::{Message, MAILBOX_CAPACITY};
 use core::marker::PhantomData;
-use microbit::hal::{
-    gpio::{Floating, Input, Pin},
-    gpiote::{GpioteChannel, GpioteChannelEvent},
+use microbit::{
+    gpio::BTN_B,
+    hal::{
+        gpio::{Floating, Input, Pin},
+        gpiote::{GpioteChannel, GpioteChannelEvent},
+    },
 };
 use rtic_sync::channel::{Sender, TrySendError};
 
@@ -10,8 +13,25 @@ pub struct Started;
 
 pub struct Stopped;
 
+pub struct GpioResources<'b> {
+    channel: GpioteChannel<'b>,
+    button: Pin<Input<Floating>>,
+}
+
+impl<'b> GpioResources<'b> {
+    pub fn new(channel: GpioteChannel<'b>, button: BTN_B) -> Self {
+        let button = button.degrade();
+        Self { channel, button }
+    }
+
+    // Due to a limitation in the HAL, it is not possible to regain access to BTN_B
+    pub fn free(self) -> Pin<Input<Floating>> {
+        self.button
+    }
+}
+
 pub struct RotationDriver<'a, 'b, S> {
-    gpiote_channel: &'b GpioteChannel<'b>,
+    gpio: &'b GpioResources<'b>,
     button_event: GpioteChannelEvent<'b, Pin<Input<Floating>>>,
     command_pipe: Sender<'a, Message, MAILBOX_CAPACITY>,
     s: PhantomData<S>,
@@ -20,18 +40,17 @@ pub struct RotationDriver<'a, 'b, S> {
 impl<'a, 'b> RotationDriver<'a, 'b, Stopped> {
     #[must_use]
     pub fn new(
-        channel: &'b GpioteChannel<'b>,
-        button: &'b Pin<Input<Floating>>,
+        resources: &'b GpioResources<'b>,
         mailbox: Sender<'a, Message, MAILBOX_CAPACITY>,
     ) -> Self {
-        channel.reset_events();
+        resources.channel.reset_events();
 
-        let event = channel.input_pin(button);
+        let event = resources.channel.input_pin(&resources.button);
         event.disable_interrupt();
         event.hi_to_lo();
 
         Self {
-            gpiote_channel: channel,
+            gpio: resources,
             button_event: event,
             command_pipe: mailbox,
             s: PhantomData,
@@ -42,7 +61,7 @@ impl<'a, 'b> RotationDriver<'a, 'b, Stopped> {
     pub fn start(self) -> RotationDriver<'a, 'b, Started> {
         self.button_event.enable_interrupt();
         RotationDriver {
-            gpiote_channel: self.gpiote_channel,
+            gpio: self.gpio,
             button_event: self.button_event,
             command_pipe: self.command_pipe,
             s: PhantomData,
@@ -54,9 +73,9 @@ impl<'a, 'b> RotationDriver<'a, 'b, Started> {
     #[must_use]
     pub fn stop(self) -> RotationDriver<'a, 'b, Stopped> {
         self.button_event.disable_interrupt();
-        self.gpiote_channel.reset_events();
+        self.gpio.channel.reset_events();
         RotationDriver {
-            gpiote_channel: self.gpiote_channel,
+            gpio: self.gpio,
             button_event: self.button_event,
             command_pipe: self.command_pipe,
             s: PhantomData,
@@ -64,7 +83,17 @@ impl<'a, 'b> RotationDriver<'a, 'b, Started> {
     }
 
     pub fn handle_button_event(&mut self) -> Result<(), TrySendError<Message>> {
-        self.gpiote_channel.reset_events();
-        self.command_pipe.try_send(Message::BtnBPress)
+        // We have to check the channel for having a pending event, because of the way
+        // the Gpiote peripheral works: there is a single Gpiote IRQ which gets pended
+        // if there is an event _on any_ of the available channels.
+        // For details, see
+        // https://infocenter.nordicsemi.com/topic/ps_nrf52833/gpiote.html?cp=5_1_0_5_8
+        if self.gpio.channel.is_event_triggered() {
+            self.gpio.channel.reset_events();
+            self.command_pipe.try_send(Message::BtnBPress)
+        } else {
+            // event does not belong to our channel -> ignore it
+            Ok(())
+        }
     }
 }
