5eccc78734283c052c674ad4c709b5510f3b417b
90degs2infty <90degs2infty@posteo.org>
2023-12-10 12:11:03 +0100

    Try to access accel irq

--- src/bin/minimal.rs
+++ src/bin/minimal.rs
@@ -16,17 +16,21 @@ mod app {
         display::nonblocking::{Display, Frame, MicrobitFrame},
         hal::{
             gpiote::Gpiote,
-            prelude::_embedded_hal_timer_CountDown,
+            prelude::{InputPin, _embedded_hal_timer_CountDown},
             timer::{Instance, Periodic, Timer},
         },
         pac::{
             NVIC, TIMER0 as LowLevelDisplayDriver, TIMER1 as HighLevelDisplayDriver,
-            TIMER2 as TimerGameDriver,
+            TIMER2 as TimerGameDriver, TWIM0 as HorizontalDriver,
         },
         Board,
     };
     use microtile_app::{
         device::{
+            accel::{
+                GpioResources as HorizontalIrqResources, HorizontalMovementDriver,
+                Started as HorizontalStarted,
+            },
             button::{GpioResources, RotationDriver, Started as RotationStarted},
             display::GridRenderer,
             timer::{GameTickDriver, Started as TickStarted},
@@ -69,6 +73,12 @@ mod app {
         game_driver: &'static mut GameDriver<'static, GameObserver>,
         timer_handler: &'static mut GameTickDriver<'static, TimerGameDriver, TickStarted>,
         rotation_handler: &'static mut RotationDriver<'static, 'static, RotationStarted>,
+        horizontal_handler: &'static mut HorizontalMovementDriver<
+            'static,
+            'static,
+            HorizontalDriver,
+            HorizontalStarted,
+        >,
     }
 
     #[init(local = [
@@ -77,7 +87,9 @@ mod app {
         timer_handler_mem: MaybeUninit<GameTickDriver<'static, TimerGameDriver, TickStarted>> = MaybeUninit::uninit(),
         gpiote_mem: MaybeUninit<Gpiote> = MaybeUninit::uninit(),
         rotation_resources_mem: MaybeUninit<GpioResources<'static>> = MaybeUninit::uninit(),
-        rotation_handler_mem: MaybeUninit<RotationDriver<'static, 'static, RotationStarted>> = MaybeUninit::uninit()
+        rotation_handler_mem: MaybeUninit<RotationDriver<'static, 'static, RotationStarted>> = MaybeUninit::uninit(),
+        horizontal_resources_mem: MaybeUninit<HorizontalIrqResources<'static>> = MaybeUninit::uninit(),
+        horizontal_handler_mem: MaybeUninit<HorizontalMovementDriver<'static, 'static, HorizontalDriver, HorizontalStarted>> = MaybeUninit::uninit()
     ])]
     fn init(cx: init::Context) -> (Shared, Local) {
         defmt::info!("init");
@@ -88,18 +100,39 @@ mod app {
 
         let (sender, receiver) = cx.local.game_driver_channel.split();
 
+        cx.local.gpiote_mem.write(Gpiote::new(board.GPIOTE));
+        let gpiote = unsafe { cx.local.gpiote_mem.assume_init_mut() };
+
+        let irq_line = board.pins.p0_25.into_pullup_input();
+        cx.local
+            .horizontal_resources_mem
+            .write(HorizontalIrqResources::new(gpiote.channel1(), irq_line));
+        let horizontal_resources: &'static mut HorizontalIrqResources<'static> =
+            unsafe { cx.local.horizontal_resources_mem.assume_init_mut() };
+
+        let mut delay = Timer::new(board.TIMER2);
+        cx.local.horizontal_handler_mem.write(
+            HorizontalMovementDriver::new(
+                horizontal_resources,
+                sender.clone(),
+                board.TWIM0,
+                board.i2c_internal,
+                &mut delay,
+            )
+            .start(),
+        );
+        let horizontal_handler = unsafe { cx.local.horizontal_handler_mem.assume_init_mut() };
+
         cx.local
             .game_driver_mem
             .write(GameDriver::new(receiver, observer));
         let game_driver = unsafe { cx.local.game_driver_mem.assume_init_mut() };
 
         cx.local.timer_handler_mem.write(
-            GameTickDriver::new(sender.clone(), board.buttons.button_a, board.TIMER2).start(),
+            GameTickDriver::new(sender.clone(), board.buttons.button_a, delay.free()).start(),
         );
         let timer_handler = unsafe { cx.local.timer_handler_mem.assume_init_mut() };
 
-        cx.local.gpiote_mem.write(Gpiote::new(board.GPIOTE));
-        let gpiote = unsafe { cx.local.gpiote_mem.assume_init_mut() };
         cx.local.rotation_resources_mem.write(GpioResources::new(
             gpiote.channel0(),
             board.buttons.button_b,
@@ -137,6 +170,7 @@ mod app {
                 game_driver,
                 timer_handler,
                 rotation_handler,
+                horizontal_handler,
             },
         )
     }
@@ -210,8 +244,9 @@ mod app {
         defmt::trace!("leaving frame update");
     }
 
-    #[task(binds = GPIOTE, priority = 4, local = [ rotation_handler ])]
+    #[task(binds = GPIOTE, priority = 4, local = [ rotation_handler, horizontal_handler ])]
     fn rotate_tile(cx: rotate_tile::Context) {
+        // TODO: pull out detection of who is responsible for the event handling?
         match cx.local.rotation_handler.handle_button_event() {
             Ok(()) => {}
             Err(TrySendError::Full(_)) => {
@@ -219,5 +254,12 @@ mod app {
             }
             Err(TrySendError::NoReceiver(_)) => unreachable!(),
         };
+        match cx.local.horizontal_handler.handle_accel_event() {
+            Ok(()) => {}
+            Err(TrySendError::Full(_)) => {
+                defmt::debug!("dropping horizontal tile movement to allow the engine to catch up");
+            }
+            Err(TrySendError::NoReceiver(_)) => unreachable!(),
+        }
     }
 }
--- src/device/accel.rs
+++ src/device/accel.rs
@@ -1,38 +1,152 @@
 use crate::game::{Message, MAILBOX_CAPACITY};
-use core::marker::PhantomData;
-use lsm303agr::{interface::I2cInterface, mode::MagOneShot, Lsm303agr};
+use core::{fmt::Debug, marker::PhantomData};
+use lsm303agr::{
+    interface::{I2cInterface, ReadData, WriteData},
+    mode::MagOneShot,
+    AccelMode, AccelOutputDataRate, AccelScale, Error, Interrupt, Lsm303agr,
+};
+use microbit::{
+    hal::{
+        gpio::{p0::P0_25, Input, Pin, PullUp},
+        gpiote::{GpioteChannel, GpioteChannelEvent},
+        prelude::{InputPin, _embedded_hal_blocking_delay_DelayUs as DelayUs},
+        twim::{Instance, Pins, Twim},
+    },
+    pac::twim0::frequency::FREQUENCY_A,
+};
 use rtic_sync::channel::{Sender, TrySendError};
 
 pub struct Started;
 
 pub struct Stopped;
 
-pub struct HorizontalMovementDriver<'a, I2C, S> {
+// TODO: merge with GpioResources from button
+pub struct GpioResources<'b> {
+    channel: GpioteChannel<'b>,
+    i2c_irq: Pin<Input<PullUp>>,
+}
+
+impl<'b> GpioResources<'b> {
+    #[must_use]
+    pub fn new(channel: GpioteChannel<'b>, i2c_irq: P0_25<Input<PullUp>>) -> Self {
+        let i2c_irq = i2c_irq.degrade();
+        Self { channel, i2c_irq }
+    }
+
+    // Due to a limitation in the HAL, it is not possible to regain access to BTN_B
+    #[must_use]
+    pub fn free(self) -> Pin<Input<PullUp>> {
+        self.i2c_irq
+    }
+}
+
+pub struct HorizontalMovementDriver<'a, 'b, T, S> {
     command_pipe: Sender<'a, Message, MAILBOX_CAPACITY>,
-    accel: Lsm303agr<I2cInterface<I2C>, MagOneShot>,
+    accel: Lsm303agr<I2cInterface<Twim<T>>, MagOneShot>,
+    i2c_irq: &'b GpioResources<'b>, // TODO: why not own it?
+    irq_event: GpioteChannelEvent<'b, Pin<Input<PullUp>>>,
     s: PhantomData<S>,
 }
 
-impl<'a, I2C> HorizontalMovementDriver<'a, I2C, Stopped> {
-    pub fn new(mailbox: Sender<'a, Message, MAILBOX_CAPACITY>, i2c: I2C) -> Self {
-        todo!()
+impl<'a, 'b, T> HorizontalMovementDriver<'a, 'b, T, Stopped> {
+    // TODO: add pin for interrupt?
+    pub fn new<D, P>(
+        irq: &'b GpioResources<'b>,
+        mailbox: Sender<'a, Message, MAILBOX_CAPACITY>,
+        twim: T,
+        bus_pins: P,
+        delay: &mut D,
+    ) -> Self
+    where
+        D: DelayUs<u32>,
+        T: Instance,
+        P: Into<Pins>,
+    {
+        irq.channel.reset_events();
+
+        let event = irq.channel.input_pin(&irq.i2c_irq);
+        event.disable_interrupt();
+        event.hi_to_lo();
+
+        let i2c = { Twim::new(twim, bus_pins.into(), FREQUENCY_A::K100) };
+        let mut accel = Lsm303agr::new_with_i2c(i2c);
+        accel.init().expect("Failed to initialize accelerometer");
+
+        accel
+            .acc_disable_interrupt(Interrupt::DataReady1)
+            .expect("Failed to disable the DRY1 interrupt");
+
+        accel
+            .set_accel_mode_and_odr(delay, AccelMode::Normal, AccelOutputDataRate::Hz25)
+            .expect("Failed to set accelerometer mode and odr");
+        accel
+            .set_accel_scale(AccelScale::G2)
+            .expect("Failed to set accelerometer scale");
+
+        Self {
+            command_pipe: mailbox,
+            accel,
+            i2c_irq: irq,
+            irq_event: event,
+            s: PhantomData,
+        }
     }
 
-    pub fn start() -> HorizontalMovementDriver<'a, I2C, Started> {
-        todo!()
+    pub fn start<CommE, PinE>(mut self) -> HorizontalMovementDriver<'a, 'b, T, Started>
+    where
+        I2cInterface<Twim<T>>:
+            ReadData<Error = Error<CommE, PinE>> + WriteData<Error = Error<CommE, PinE>>,
+        CommE: Debug,
+        PinE: Debug,
+    {
+        self.irq_event.enable_interrupt();
+
+        self.accel
+            .acc_enable_interrupt(Interrupt::DataReady1)
+            .expect("Failed to enable accel interrupt");
+        HorizontalMovementDriver {
+            command_pipe: self.command_pipe,
+            accel: self.accel,
+            i2c_irq: self.i2c_irq,
+            irq_event: self.irq_event,
+            s: PhantomData,
+        }
     }
 
-    pub fn free() -> I2C {
-        todo!()
+    pub fn free(self) -> (T, Pins)
+    where
+        T: Instance,
+    {
+        self.accel.destroy().free()
     }
 }
 
-impl<'a, I2C> HorizontalMovementDriver<'a, I2C, Started> {
-    pub fn stop() -> HorizontalMovementDriver<'a, I2C, Stopped> {
+impl<'a, 'b, T> HorizontalMovementDriver<'a, 'b, T, Started> {
+    pub fn stop() -> HorizontalMovementDriver<'a, 'b, T, Stopped> {
         todo!()
     }
 
-    pub fn handle_accel_event() {
-        todo!()
+    pub fn handle_accel_event<CommE, PinE>(&mut self) -> Result<(), TrySendError<Message>>
+    where
+        I2cInterface<Twim<T>>:
+            ReadData<Error = Error<CommE, PinE>> + WriteData<Error = Error<CommE, PinE>>,
+        CommE: Debug,
+        PinE: Debug,
+    {
+        // We have to check the channel for having a pending event, because of the way
+        // the Gpiote peripheral works: there is a single Gpiote IRQ which gets pended
+        // if there is an event _on any_ of the available channels.
+        // For details, see
+        // https://infocenter.nordicsemi.com/topic/ps_nrf52833/gpiote.html?cp=5_1_0_5_8
+        if self.i2c_irq.channel.is_event_triggered() {
+            self.i2c_irq.channel.reset_events();
+            defmt::warn!("Accel event");
+            self.accel.acceleration().unwrap();
+            //self.command_pipe.try_send(Message::BtnBPress)
+            Ok(())
+        } else {
+            // event does not belong to our channel -> ignore it
+            Ok(())
+        }
     }
 }
