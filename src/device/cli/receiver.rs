d80b99aa5668626105fdc7d024da8c19fcefd6a1
90degs2infty <90degs2infty@posteo.org>
2023-12-20 13:57:23 +0100

    Implement help command (untested)

--- src/device/cli/receiver.rs
+++ src/device/cli/receiver.rs
@@ -1,19 +1,75 @@
-use rtic_sync::channel::Receiver;
+use super::{
+    command::Command,
+    downlink::MAILBOX_CAPACITY as IN_CAPACITY,
+    uplink::{Message, MAILBOX_CAPACITY as OUT_CAPACITY},
+};
+use crate::util::StringIter;
+use rtic_sync::channel::{Receiver, Sender};
 
-use super::{command::Command, downlink::MAILBOX_CAPACITY};
+pub enum DriverError {
+    DownlinkSenderDropped,
+    UplinkReceiverDropped,
+    Encoding,
+}
 
 pub struct CommandReceiver {
-    _mailbox: Receiver<'static, Command, MAILBOX_CAPACITY>,
+    incoming: Receiver<'static, Command, IN_CAPACITY>,
+    outgoing: Sender<'static, Message, OUT_CAPACITY>,
 }
 
 impl CommandReceiver {
     #[must_use]
-    pub fn new(mailbox: Receiver<'static, Command, MAILBOX_CAPACITY>) -> Self {
-        Self { _mailbox: mailbox }
+    pub fn new(
+        incoming: Receiver<'static, Command, IN_CAPACITY>,
+        outgoing: Sender<'static, Message, OUT_CAPACITY>,
+    ) -> Self {
+        Self { incoming, outgoing }
     }
 
-    #[allow(clippy::unused_async)]
-    pub async fn run(&mut self) {
-        todo!()
+    pub async fn run(&mut self) -> Result<(), DriverError> {
+        loop {
+            let cmd = self
+                .incoming
+                .recv()
+                .await
+                .map_err(|_| DriverError::DownlinkSenderDropped)?;
+            self.execute(cmd).await?;
+        }
+    }
+
+    async fn execute(&mut self, cmd: Command) -> Result<(), DriverError> {
+        match cmd {
+            Command::Help => self.execute_help().await,
+            Command::Version => todo!(),
+        }
+    }
+
+    async fn execute_help(&mut self) -> Result<(), DriverError> {
+        let help_text = StringIter::<'_, 32>::from(
+            "\r\n\
+            === microtile ===\r\n\
+            \r\n\
+            available commands:\r\n\
+            - help - prints this help message\r\n\
+            - version - prints VCS information\r\n\
+            \r\n\
+            syntax:\r\n\
+            $ <cmd>;
+            where <cmd> is one of above commands\r\n\
+            \r\n\
+            ==================\r\n",
+        );
+
+        for msg in help_text {
+            match msg {
+                Ok(msg) => self
+                    .outgoing
+                    .send(msg)
+                    .await
+                    .map_err(|_| DriverError::UplinkReceiverDropped)?,
+                Err(_) => unreachable!("Hardcoded string should be convertible"),
+            }
+        }
+        Ok(())
     }
 }
