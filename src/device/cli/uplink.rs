be9570d5cc5e2e0f351b9a12d5f48ca7f7cd1173
90degs2infty <90degs2infty@posteo.org>
2023-12-23 15:10:04 +0100

    Sketch out `UplinkDriver` API

--- src/device/cli/uplink.rs
+++ src/device/cli/uplink.rs
@@ -1,6 +1,34 @@
 use heapless::String;
+use microbit::hal::uarte::{Instance, UarteTx};
+use rtic_sync::channel::Receiver;
 
 pub const MESSAGE_LENGTH: usize = 32;
 pub type Message = String<MESSAGE_LENGTH>;
 
 pub const MAILBOX_CAPACITY: usize = 32;
+
+pub enum DriverError {
+    Other,
+}
+
+pub struct UplinkDriver<T>
+where
+    T: Instance,
+{
+    _tx: UarteTx<T>,
+    _mailbox: Receiver<'static, Message, MAILBOX_CAPACITY>,
+}
+
+impl<T> UplinkDriver<T>
+where
+    T: Instance,
+{
+    #[must_use]
+    pub fn new(_tx: UarteTx<T>, _mailbox: Receiver<'static, Message, MAILBOX_CAPACITY>) -> Self {
+        todo!()
+    }
+
+    pub async fn run(&mut self) -> Result<(), DriverError> {
+        todo!()
+    }
+}
